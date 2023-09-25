ba7be3729242a88c3bde3afd5ae02a3bcdaac74a
djlynn03 <djlynn03@gmail.com>
2023-09-25 01:43:03 -0500

    revamp

--- script.js
+++ script.js
@@ -1,49 +1,21 @@
-function resize(){
-    var head = document.getElementById('header');
-    var foot = document.getElementById('footer');
-    var desc = document.getElementById('description');
-    desc.style.height = window.innerHeight + 'px';
+var gridlines = [];
+
+function drawGrid() {
+  var grid = document.getElementById("grid");
+
+  let w = window.innerWidth;
+  let h = window.innerHeight;
+
+  var path;
+
+  for (let i = 0; i < 10; i++) {
+    path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute(
+      "d",
+      "M 1,97.857143 C 19.285714,96.428571 24.016862,131.64801 90.714286,132.85714 140.78762,133.7649 202.79376,66.16041 202.79376,66.16041"
+    );
+    grid.appendChild(path);
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function (event) {
-    var head = document.getElementById('header');
-    var foot = document.getElementById('footer');
-    var desc = document.getElementById('description');
-    desc.style.height = window.innerHeight + 'px';
-
-    var dataText = ["Hello!", "My name is Duncan", "Check out some of my projects and interests:"];
-
-    function typeWriter(text, i, fnCallback) {
-        if (i < (text.length)) {
-            document.querySelector("#header").innerHTML = text.substring(0, i + 1) + '<span class="cursor-blink" aria-hidden="true"></span>';
-
-            setTimeout(function () {
-                typeWriter(text, i + 1, fnCallback)
-            }, 20);
-        }
-        else if (typeof fnCallback == 'function') {
-            setTimeout(fnCallback, 350);
-        }
-    }
-    function showDescription() {
-        document.querySelector("#description").style.display = "block";
-        document.querySelector("#description").animate([{'height': 0}, {'height': '100%'}], {duration: 400});
-        document.querySelector("#description").style.opacity = 1;
-    }
-    function StartTextAnimation(i) {
-
-        if (dataText[i] == undefined){
-            showDescription();
-            return;
-        }
-        if (i < dataText[i].length) {
-            typeWriter(dataText[i], 0, function () {
-                StartTextAnimation(i + 1);
-            });
-        } else {
-            showDescription();
-        }
-    }
-    StartTextAnimation(0);
-
-});
+// document.addEventListener("DOMContentLoaded", drawGrid());
